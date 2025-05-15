0545f7dd6c4558b5fff3e77ca23696946ab77581
Irina Stepanova <arumirinka@github.com>
2025-05-16 05:56:54 +0600

    feat: add server error handling

--- src/handler.ts
+++ src/handler.ts
@@ -4,7 +4,13 @@ import { routes } from "./routes";
 export const handler = (request: IncomingMessage, response: ServerResponse) => {
   const currentRoute = routes.find((route) => route.method === request.method && request.url?.startsWith(route.path));
   if (currentRoute) {
-    currentRoute.action(request, response);
+    try {
+      currentRoute.action(request, response);
+    } catch (error) {      
+      response.writeHead(500, { 'Content-Type': 'application/json' });
+      response.write(JSON.stringify(`Internal server error: ${error}`));
+      response.end();
+    }
   } else  {
     response.writeHead(404, { 'Content-Type': 'application/json' });
     response.write(JSON.stringify(`Requested url ${request.url} was not found.`));
