8a91a3163c6912a8b150b911a1dd9ae3915bc709
Irina Stepanova <arumirinka@github.com>
2025-05-19 00:10:57 +0600

    feat: improve user validation

--- src/utils.ts
+++ src/utils.ts
@@ -1,7 +1,17 @@
 import { User } from "types";
 
 export const isValidUser = (user: User) => {
-  if (!user || !user.username || !user.age || !user.hobbies) {
+  if (
+    !user
+    || typeof user.username !== "string"
+    || !user.username?.trim()
+    || !user.age
+    || typeof user.age !== "number"
+    || user.age < 0
+    || !user.hobbies
+    || !Array.isArray(user.hobbies)
+    || !user.hobbies.every((hobby) => typeof hobby === "string" && !!hobby?.trim())
+  ) {
     return false;
   } else {
     return true;
